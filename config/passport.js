9b2444b96d5f353df1b2103ad104d7f0af3ef8fa
Carlos Romo <79283213+CarlosRomo0806@users.noreply.github.com>
2021-11-16 21:12:14 -0600

    passport update

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,11 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const User = require('../src/models/user');
+const Database = require('../src/models/database');
+const jwt = require('jsonwebtoken');
+const TokensController = require('../src/controllers/tokens.controller');
+const StudentController = require('../src/controllers/students.controller');
+const TeacherController = require('../src/controllers/teachers.controller');
+
+require('dotenv').config();
 
 module.exports = function (passport) {
     passport.use(new GoogleStrategy({
@@ -8,28 +13,52 @@ module.exports = function (passport) {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google/callback'
         },
+        
         async (accessToken, refreshToken, profile, done) => {
             const newUser = {
+                id: ""+ Math.random().toString(36).substr(2, 9),
                 googleId: profile.id,
-                displayName: profile.displayName,
-                firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                image: profile.photos[0].value
+                userName: profile.displayName,
+                email: profile.emails[0].value,
+                role: 'student'
             }
 
             try{
                 let user = await User.findOne({googleId: profile.id})
 
                 if(user){
-                    done(null, user)
+                    
                 } else{
-                    user = await User.create(newUser)
-                    done(null, user)
+                    user = database.insertOne(newUser).then(response => {
+                        if(role == "student"){
+                            StudentController.signUser(id)
+                        }else if(role == "teacher"){
+                            TeacherController.signUser(id, req.body.salary)
+                        }
+                        res.statusMessage = "User created correctly!";
+                        return res.status(201).end();
+                    })
+                        .catch(error => {
+                        res.statusMessage = "User already exist!";
+                        return res.status(400).end();
+                    });
+                    
                 }
             } catch(err){
                 console.error(err)
+                return
             }
-        }
+            let token = jwt.sign(response, secret,{ expiresIn: '1h' });
+                TokensController.newToken(token, results.id);
+                res.statusMessage = "Login sucess";
+                res.status(200).send({
+                    "email": user.email,
+                    "role": user.role,
+                    "token": user.token
+                });
+                done(null, user)
+                
+        } 
     ))
 
     passport.serializeUser((user, done) => {
