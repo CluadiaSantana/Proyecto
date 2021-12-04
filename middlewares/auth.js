3973e2faa06ea0912384f33bd45a44404dfff6ac
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-12-04 08:34:51 -0600

    commented code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,26 @@
+/*JWT
+Compact, url safe means of representing claims to be tranferred between two parties. 
+claims are encoded as JSON object
+claim: piece of info asserted about a subject. represented as name/value pairs.
+*/
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 let secret = process.env.JWTSECRET;
 
+/*JWT.SECRET
+If callback is supplied, function acts asynchronously. secret can be a function that should fecth the secret or public key.
+TOKEN: JsonWebToken string
+SECRET: string or buffer containing either the secret for HMAC algorithms,
+OR PEM encoded public key.
+
+*/
 function authPer(req,res,next){
     let token= req.headers["x-auth"];
     if(token){ 
         jwt.verify(token, secret, (err, payload)=>{
             if(err){
-                res.status(401).send({error: "Token no válido"})   
+                res.status(401).send({error: "Invalid Token"})   
             }else{
                 req.role = payload.role;
                 req.id=payload.id;
@@ -15,7 +28,7 @@ function authPer(req,res,next){
             }
         })
     }else{
-        res.status(401).send({error: "falta token"})
+        res.status(401).send({error: "Token Missing!"})
     }
 
     
